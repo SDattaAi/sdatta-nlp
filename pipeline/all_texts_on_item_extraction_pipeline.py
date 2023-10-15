edfd5d41183f9cbe1bc9e377cd6ec9f9eb8daf9b
orannahum <oranne5@gmail.com>
2023-10-15 15:51:22 +0300

    add text from url 1.

--- pipeline/all_texts_on_item_extraction_pipeline.py
+++ pipeline/all_texts_on_item_extraction_pipeline.py
@@ -1,24 +1,47 @@
 from vision_and_nlp_models.utils import translate_to_english, get_from_url_palmers
 import pandas as pd
+from googlesearch import search
+import warnings
+warnings.filterwarnings("ignore")
 
-item = '100549321000'
-url = 'https://www.palmers.at/smart-shirt-' + item + '.html'
-title, description, materials = get_from_url_palmers(url)
-text_to_translate = title + " " + description + "\n" + materials
-translated_text = translate_to_english(text_to_translate)
-print(f"Original Text: {text_to_translate}")
-print(f"Translated Text(en): {translated_text}")
-
+fashion_items = pd.read_csv('/Users/guybasson/Desktop/sdatta-nlp/fashion_items.csv').drop('Unnamed: 0', axis=1)
 artikelstamm_df = pd.read_csv('/Users/guybasson/Desktop/sdatta-nlp/l_artikelstamm.csv', sep=';')
 
-#print all columns
-pd.set_option('display.max_columns', None)
-rows_with_item = artikelstamm_df[artikelstamm_df['sammelartikel'] == str(item)]
-desc_from_artikelstamm = rows_with_item['wgrbez_en'].values[0]
-colors = rows_with_item['color'].unique()
-sizes = rows_with_item['size'].unique()
+fashion_items['item'] = fashion_items['sku'].astype(str).str[0:12]
+fashion_items = fashion_items[fashion_items['item'].isin(artikelstamm_df['sammelartikel'].unique())]
+print(fashion_items['item'].nunique())
 
-print(f"Description from Artikelstamm: {desc_from_artikelstamm}")
-print(f"Colors: {colors}")
-print(f"Sizes: {sizes}")
+for item in fashion_items['item'].unique()[:5]:
+    print("item", item)
+    query = str(item) + " palmers"
+    for url in search(query, num_results=5):
+        if ("palmers" in url) and (str(item) in url):
+            break
+        else:
+            url = None
+    print("final url", url)
+    title = ""
+    description = ""
+    materials = ""
+    if url is not  None:
+      title, description, materials = get_from_url_palmers(url)
+    text_to_translate = ''
+    if title is not None:
+        text_to_translate = text_to_translate + title + " "
+    if description is not None:
+        text_to_translate = text_to_translate + description + " "
+    if materials is not None:
+        text_to_translate = text_to_translate + materials + " "
+    translated_text = translate_to_english(text_to_translate)
+    print(f"Translated Text(en): {translated_text}")
+    #print all columns
+    pd.set_option('display.max_columns', None)
+    rows_with_item = artikelstamm_df[artikelstamm_df['sammelartikel'] == str(item)]
+    desc_from_artikelstamm = rows_with_item['wgrbez_en'].values[0]
+    colors = rows_with_item['color'].unique()
+    sizes = rows_with_item['size'].unique()
+
+    print(f"Description from Artikelstamm: {desc_from_artikelstamm}")
+    print(f"Colors: {colors}")
+    print(f"Sizes: {sizes}")
 
--- vision_and_nlp_models/utils.py
+++ vision_and_nlp_models/utils.py
@@ -6,46 +6,40 @@ import requests
 from bs4 import BeautifulSoup
 
 def get_from_url_palmers(url):
-    try:
-        # Send a GET request to the URL to fetch the webpage content
-        response = requests.get(url)
+    title = None
+    description = None
+    materials = None
+    # Send a GET request to the URL to fetch the webpage content
+    response = requests.get(url)
 
-        # Check if the request was successful (status code 200)
-        if response.status_code == 200:
-            # Parse the HTML content of the webpage using BeautifulSoup
-            soup = BeautifulSoup(response.text, 'html.parser')
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the HTML content of the webpage using BeautifulSoup
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-            # Find the div element with class "value" and itemprop "description"
-            div_element_desc = soup.find('div', class_='value', itemprop='description')
-            div_element_title = soup.find('h1', class_='page-title', itemprop='name')
-            material_data = soup.find('td', {'class': 'col data', 'data-th': 'Material'})
+        # Find the div element with class "value" and itemprop "description"
+        div_element_desc = soup.find('div', class_='value', itemprop='description')
+        div_element_title = soup.find('h1', class_='page-title', itemprop='name')
+        material_data = soup.find('td', {'class': 'col data', 'data-th': 'Material'})
 
-            # Check if the description div element was found
-            if div_element_desc:
-                # Extract the text value from the div element
-                description = div_element_desc.text.strip()
-                print("Description:", description)
-            else:
-                print("Description element not found.")
+        # Check if the description div element was found
+        if div_element_desc:
+            # Extract the text value from the div element
+            description = div_element_desc.text.strip()
 
-            # Check if the title h1 element was found
-            if div_element_title:
-                # Extract the text value from the h1 element
-                title = div_element_title.text.strip()
-                print("Title:", title)
-            else:
-                print("Title element not found.")
-            if material_data:
-                # Extract the text value from the h1 element
-                materials = material_data.text.strip()
-                print("Materials:", materials)
-            else:
-                print("Materials element not found.")
-        else:
-            print("Failed to fetch webpage. Status code:", response.status_code)
 
-    except Exception as e:
-        print("An error occurred:", str(e))
+        # Check if the title h1 element was found
+        if div_element_title:
+            # Extract the text value from the h1 element
+            title = div_element_title.text.strip()
+
+        if material_data:
+            # Extract the text value from the h1 element
+            materials = material_data.text.strip()
+
+    else:
+        print("Failed to fetch webpage. Status code:", response.status_code)
+
 
     return title, description, materials
 
