6c11f7c500b41e4228598a75d0cf02e3d9f49351
orannahum <oranne5@gmail.com>
2023-10-15 13:06:11 +0300

    add text from url 1.1

--- pipeline/all_texts_on_item_extraction_pipeline.py
+++ pipeline/all_texts_on_item_extraction_pipeline.py
@@ -1,11 +1,14 @@
 from vision_and_nlp_models.utils import translate_to_english, get_from_url_palmers
-item = '100549321000'
-url = 'https://www.palmers.at/smart-shirt- ' + item + '.html'
+import pandas as pd
 
+item = '100549321000'
+url = 'https://www.palmers.at/smart-shirt-' + item + '.html'
 title, description, materials = get_from_url_palmers(url)
 text_to_translate = title + " " + description + "\n" + materials
 translated_text = translate_to_english(text_to_translate)
 print(f"Original Text: {text_to_translate}")
 print(f"Translated Text(en): {translated_text}")
 
+artikelstamm_df = pd.read_csv('/Users/guybasson/Desktop/sdatta-nlp/l_artikelstamm.csv', sep=';')
+print(artikelstamm_df.head())
 
--- pipeline/get_from_url.py
+++ pipeline/get_from_url.py
@@ -1,42 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
+from lxml import html
 
-# Define the URL of the webpage
-url = 'https://www.palmers.at/urban-nights-pyjama-100639031000.html'
-def get_from_url_palmers(url):
-    try:
-        # Send a GET request to the URL to fetch the webpage content
-        response = requests.get(url)
-
-        # Check if the request was successful (status code 200)
-        if response.status_code == 200:
-            # Parse the HTML content of the webpage using BeautifulSoup
-            soup = BeautifulSoup(response.text, 'html.parser')
-
-            # Find the div element with class "value" and itemprop "description"
-            div_element_desc = soup.find('div', class_='value', itemprop='description')
-            div_element_title = soup.find('h1', class_='page-title', itemprop='name')
-
-            # Check if the description div element was found
-            if div_element_desc:
-                # Extract the text value from the div element
-                description = div_element_desc.text.strip()
-                print("Description:", description)
-            else:
-                print("Description element not found.")
-
-            # Check if the title h1 element was found
-            if div_element_title:
-                # Extract the text value from the h1 element
-                title = div_element_title.text.strip()
-                print("Title:", title)
-            else:
-                print("Title element not found.")
-        else:
-            print("Failed to fetch webpage. Status code:", response.status_code)
-
-    except Exception as e:
-        print("An error occurred:", str(e))
+url = 'https://www.palmers.at/smart-shirt-100549321000.html'
 
 
-    return title, description
+response = requests.get(url)
+tree = html.fromstring(response.content)
+element = tree.xpath('/html/body/div[2]/main/div[2]/div/div[1]/div[3]/form/div[1]/div[1]/div/div[3]')
+
+if element:
+    print(element[0].text_content())
+else:
+    print("Element not found.")
+
+
