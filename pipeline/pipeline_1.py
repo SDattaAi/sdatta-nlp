2c7157003239a2ac378fce1bec0c1bc5af7834cb
orannahum <oranne5@gmail.com>
2023-11-01 15:29:17 +0200

    add text from url 1.81

--- pipeline/pipeline_1.py
+++ pipeline/pipeline_1.py
@@ -11,7 +11,7 @@ import warnings
 warnings.filterwarnings("ignore")
 # %%
 # inputs :
-yolo_threshold = 0.9
+yolo_threshold = 0.5
 item_name = "zara_item_1"
 image_path = "/Users/guybasson/Desktop/sdatta-nlp/photos/" + item_name + ".jpg"
 description_path = "/Users/guybasson/Desktop/sdatta-nlp/descriptions/" + item_name + ".txt"
@@ -36,11 +36,11 @@ outputs = yolo_model(**inputs)
 probs = outputs.logits.softmax(-1)[0, :, :-1]
 keep = probs.max(-1).values > yolo_threshold
 bboxes_scaled = rescale_bboxes(outputs.pred_boxes[0, keep].cpu(), image.size)
-plot_results(image, probs[keep], bboxes_scaled, yolo_cats)
+#plot_results(image, probs[keep], bboxes_scaled, yolo_cats)
 if len(bboxes_scaled) == 0:
     print("No items found, take the whole image")
     for key in dict_of_clip_texts.keys():
-        prob = clip_results(dict_of_clip_texts[key], image, clip_model, clip_processor)
+        clip_results(dict_of_clip_texts[key], image, clip_model, clip_processor)
 else:
     print("Found ", len(bboxes_scaled), " items")
     i = 0
@@ -50,14 +50,15 @@ else:
         cl = p.argmax()
         cl = p.argmax()
         cat = yolo_cats[cl]
-
-        print("item:", i, " cat:", cat)
-        cropped_img = image.crop((xmin, ymin, xmax, ymax))
-        cropped_img = cropped_img.resize((224, 224))
-        plt.imshow(cropped_img)
-        plt.title("i: " + str(i) + " yolo cat: " + yolo_cats[cl])
-        plt.show()
-        for key in dict_of_clip_texts.keys():
-            prob = clip_results(dict_of_clip_texts[key], cropped_img, clip_model, clip_processor)
+        if cat not in ['neckline', 'sleeve']:
+            print("item:", i, " cat:", cat)
+            cropped_img = image.crop((xmin, ymin, xmax, ymax))
+            cropped_img = cropped_img.resize((224, 224))
+            plt.imshow(cropped_img)
+            plt.title("i: " + str(i) + " yolo cat: " + yolo_cats[cl])
+            plt.show()
+            for key in dict_of_clip_texts.keys():
+                clip_results(dict_of_clip_texts[key], cropped_img, clip_model, clip_processor)
 
 print("Real description of product: ", description)
+print("yolo categories: ", yolo_cats)
