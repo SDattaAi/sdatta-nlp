61b7fe72fb0f3ce4040814a40b1665ddecf2e629
orannahum <oranne5@gmail.com>
2024-01-17 16:15:31 +0200

    add tests & inputs validation 1.3

--- palmers_fashion/clearml_pipeline_controller/fashion_strategy/tests/test_update_current_stock_with_new_sku.py
+++ palmers_fashion/clearml_pipeline_controller/fashion_strategy/tests/test_update_current_stock_with_new_sku.py
@@ -5,40 +5,25 @@ current_stock1 = {'store1': {'sku1': 5}}
 start_dates1 = {'2024-01-19': [('sku1', 'store1')]}
 date1 = '2024-01-19'
 dict_stocks1 = {'store1': {'sku1': 10}}
-expected_result1 = (
-    {'store1': {'sku1': 10}},
-    {'sku1': {'store1': {'len': 0, 'sum': 0}}},
-    {'sku1': {'store1': {'len': 0, 'sum': 0}}},
-    {'sku1': {'store1': {'len': 0, 'sum': 0}}}
-)
+expected_result1 = ({'store1': {'sku1': 10}}, {'sku1': {'store1': {'len': 0, 'sum': 0}}}, {'sku1': {'store1': {'len': 0, 'sum': 0}}}, {'sku1': {'store1': {'len': 0, 'sum': 0}}})
 
-current_stock2 = {}
+current_stock2 = {'store1':{}}
 start_dates2 = {'2024-01-19': [('sku1', 'store1')]}
 date2 = '2024-01-19'
 dict_stocks2 = {'store1': {'sku1': 10}}
-expected_result2 = (
-    {'store1': {'sku1': 10}},
-    {'sku1': {'store1': {'len': 0, 'sum': 0}}},
-    {'sku1': {'store1': {'len': 0, 'sum': 0}}},
-    {'sku1': {'store1': {'len': 0, 'sum': 0}}}
-)
+expected_result2 = ({'store1': {'sku1': 10}}, {'sku1': {'store1': {'len': 0, 'sum': 0}}}, {'sku1': {'store1': {'len': 0, 'sum': 0}}}, {'sku1': {'store1': {'len': 0, 'sum': 0}}})
 
-current_stock3 = {'store1': {'sku1': 5}}
-start_dates3 = {}
+current_stock3 = {'store3': {'sku1': 5}}
+start_dates3 = {'2024-01-19':[]}
 date3 = '2024-01-19'
-dict_stocks3 = {'store1': {'sku1': 10}}
-expected_result3 = (
-    {'store1': {'sku1': 10}},
-    {'sku1': {'store1': {'len': 0, 'sum': 0}}},
-    {'sku1': {'store1': {'len': 0, 'sum': 0}}},
-    {'sku1': {'store1': {'len': 0, 'sum': 0}}}
-)
+dict_stocks3 = {'store3':{'sku1': 1000}, 'store1': {'sku1': 10}}
+expected_result3 = ({'store3': {'sku1': 5}}, {'sku1': {'store1': {'len': 0, 'sum': 0}}}, {'sku1': {'store1': {'len': 0, 'sum': 0}}}, {'sku1': {'store1': {'len': 0, 'sum': 0}}})
 
 current_stock4 = {'store1': {'sku1': 5}}
 start_dates4 = {'2024-01-19': [('sku1', 'store1')]}
 date4 = '2024-01-19'
-dict_stocks4 = {}
-expected_result4 = None
+dict_stocks4 = {'store2':{}, 'store1': {}}
+expected_result4 = ({'store1': {'sku1': 0}}, {'sku1': {'store1': {'len': 0, 'sum': 0}}}, {'sku1': {'store1': {'len': 0, 'sum': 0}}}, {'sku1': {'store1': {'len': 0, 'sum': 0}}})
 
 @pytest.mark.parametrize("current_stock, start_dates, date, dict_stocks, expected_result", [
     (current_stock1, start_dates1, date1, dict_stocks1, expected_result1),
@@ -53,5 +38,6 @@ def test_update_current_stock_with_new_sku(current_stock, start_dates, date, dic
 
     result = update_current_stock_with_new_sku(current_stock, start_dates, date, dict_stocks,
                                                Ex_total_days_wo_inv, Ex_i_s_r, avg_integral_diff)
-
     assert result == expected_result
+
+#
